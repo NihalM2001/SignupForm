bc17ca4c357703a47b04859616d3511a1353b970
nihal <nihalmurali047@gmail.com>
2023-03-23 10:48:52 +0530

    set password validation. Validation styling in progress

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const password=document.getElementById('password');
 const validate=document.getElementById('password-validation');
-const errorIcon=document.querySelector('pass-error')
+const errorIcon=document.querySelector('.pass-error');
+
 
 validate.addEventListener('blur', ()=> { validation(); });
 
@@ -9,5 +10,17 @@ function validation()
     if(password.value !== validate.value) 
     {
         console.log('hello');
+        errorIcon.innerHTML="passwords doesnt match!!";
+        validate.classList.add('errorSpan');
+        /*
+        validate.style.backgroundColor="#f3d9d9";
+        validate.style.borderColor="red";
+        errorIcon.innerHTML="passwords doesnt match. poyi chaav!";
+        */
+    }
+    else
+    {
+        errorIcon.innerHTML="";
+        validate.classList.remove('errorSpan');
     }
 }
